e67026c01ff0452dbbe1059e26eecda2de1d0231
dqe55666 <3174927613@qq.com>
2026-06-09 21:16:07 +0800

    初始化

---
+++ src/main/java/cn/org/rsrs/project/project_a3_ifly/ProjectA3IflyApplication.java
@@ -0,0 +1,13 @@
+package cn.org.rsrs.project.project_a3_ifly;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectA3IflyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProjectA3IflyApplication.class, args);
+    }
+
+}
---
+++ src/test/java/cn/org/rsrs/project/project_a3_ifly/ProjectA3IflyApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.org.rsrs.project.project_a3_ifly;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectA3IflyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
